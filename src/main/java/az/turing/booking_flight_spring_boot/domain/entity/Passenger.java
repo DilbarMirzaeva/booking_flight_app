204b16c1dd4f1aa4dc90643d734713d69cf664af
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-05-04 23:58:27 +0400

    booking and passenger entities

--- src/main/java/az/turing/booking_flight_spring_boot/domain/entity/Passenger.java
+++ src/main/java/az/turing/booking_flight_spring_boot/domain/entity/Passenger.java
@@ -1,4 +1,28 @@
 package az.turing.booking_flight_spring_boot.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "passengers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @Column(name="surname",nullable = false)
+    private String surname;
+
+    @ManyToOne
+    @JoinColumn(name = "booking_id",nullable = false)
+    private Booking booking;
 }
