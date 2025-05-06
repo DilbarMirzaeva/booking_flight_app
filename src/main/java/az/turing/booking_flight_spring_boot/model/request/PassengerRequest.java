970508c65b3e1a1cfae8a3d67d64d87c9085ce3e
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-05-06 08:23:08 +0400

    flight and passenger's request response, move package

---
+++ src/main/java/az/turing/booking_flight_spring_boot/model/request/PassengerRequest.java
@@ -0,0 +1,13 @@
+package az.turing.booking_flight_spring_boot.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PassengerRequest {
+    private String name;
+    private String surname;
+}
---
+++ src/main/java/az/turing/booking_flight_spring_boot/model/response/FlightResponse.java
@@ -0,0 +1,19 @@
+package az.turing.booking_flight_spring_boot.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FlightResponse {
+    private Long id;
+    private String origin;
+    private String destination;
+    private Integer availableSeats;
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+}
