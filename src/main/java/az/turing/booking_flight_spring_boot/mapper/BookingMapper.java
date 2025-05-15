1aa18b25db695d94e8fc72cc6927a237dffb7f17
user <alimuradof93@gmail.com>
2025-05-15 15:07:03 +0400

    booking,response,request

--- src/main/java/az/turing/booking_flight_spring_boot/mapper/BookingMapper.java
+++ src/main/java/az/turing/booking_flight_spring_boot/mapper/BookingMapper.java
@@ -1,4 +1,20 @@
 package az.turing.booking_flight_spring_boot.mapper;
 
-public class BookingMapper {
+import az.turing.booking_flight_spring_boot.domain.entity.Booking;
+import az.turing.booking_flight_spring_boot.model.request.BookingRequest;
+import az.turing.booking_flight_spring_boot.model.response.BookingResponse;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface BookingMapper extends EntityMapper<Booking, BookingResponse> {
+    @Override
+    Booking toEntity(BookingResponse bookingResponse);
+    @Override
+    BookingResponse toDto(Booking booking);
+    @Override
+    List<Booking> toListEntity(List<BookingResponse> bookingResponse);
+    @Override
+    List<BookingResponse> toListDto(List<Booking> booking);
 }
--- src/main/java/az/turing/booking_flight_spring_boot/model/response/BookingResponse.java
+++ src/main/java/az/turing/booking_flight_spring_boot/model/response/BookingResponse.java
@@ -1,4 +1,18 @@
 package az.turing.booking_flight_spring_boot.model.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingResponse {
+        private Long bookingId;
+        private Long flightId;
+        private List<PassengerResponse> passengers;
+        private Long price;
+        private Long numberofSeats;
 }
