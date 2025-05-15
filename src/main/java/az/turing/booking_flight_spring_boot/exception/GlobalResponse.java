aaaa4a8c09e556f412bf1c52ba4db9122827e74a
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-05-15 17:27:27 +0400

    flight and passenger's controllers and GlobalExceptionHandler

---
+++ src/main/java/az/turing/booking_flight_spring_boot/exception/GlobalResponse.java
@@ -0,0 +1,20 @@
+    package az.turing.booking_flight_spring_boot.exception;
+
+    import lombok.AllArgsConstructor;
+    import lombok.Builder;
+    import lombok.Data;
+    import lombok.NoArgsConstructor;
+
+    import java.time.LocalDateTime;
+    import java.util.UUID;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public class GlobalResponse {
+        private UUID uuid;
+        private String message;
+        private ErrorMessage error;
+        private LocalDateTime timestamp;
+    }
