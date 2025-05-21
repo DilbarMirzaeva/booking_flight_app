509c194ca9838cbca886967b6e1ce0a1dc0a8d70
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-05-22 03:19:39 +0400

    PassengerAOP,booking and flight some method corrections

--- src/main/java/az/turing/booking_flight_spring_boot/domain/repository/BookingRepository.java
+++ src/main/java/az/turing/booking_flight_spring_boot/domain/repository/BookingRepository.java
@@ -1,9 +1,15 @@
 package az.turing.booking_flight_spring_boot.domain.repository;
 
 import az.turing.booking_flight_spring_boot.domain.entity.Booking;
+import az.turing.booking_flight_spring_boot.domain.entity.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookingRepository extends JpaRepository<Booking,Long> {
+    Booking findBookingsByFlight(Flight flight);
+    boolean existsByFlight(Flight flight);
+    List<Booking> findAllByFlight(Flight flight);
 }
